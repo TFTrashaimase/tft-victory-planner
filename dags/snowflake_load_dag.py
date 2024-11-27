f56fd2a49e3eb33f080c92cff70a2798834cf236
mangodm-web <mangodm.web3@gmail.com>
2024-11-28 04:20:33 +0900

    wip: DAG 오류 수정 - 학성님이 코드 정리 후 다시 푸시 예정

--- dags/snowflake_load_dag.py
+++ dags/snowflake_load_dag.py
@@ -39,55 +39,51 @@ with DAG(
     load_data_to_stage = SnowflakeOperator(
         task_id="load_data_to_stage",
         snowflake_conn_id="conn_snowflake_2",
-        # sql=f"""
-        #     COPY INTO @{SNOWFLAKE_STAGE}/
-        #     FROM s3://{BUCKET_NAME}/{{{{ ds }}}}/
-        #     CREDENTIALS = (AWS_KEY_ID = '{{{{ var.value.AWS_ACCESS_KEY }}}}' AWS_SECRET_KEY = '{{{{ var.value.AWS_SECRET_KEY }}}}')
-        #     FILE_FORMAT = (TYPE = 'PARQUET');
-        # """,
-        sql=f"""
-            COPY INTO @{SNOWFLAKE_STAGE}/
-            FROM 's3://{BUCKET_NAME}/2024-11-26/'
+        sql=f"""CREATE OR REPLACE STAGE {SNOWFLAKE_SCHEMA}.{SNOWFLAKE_STAGE}
+            URL='s3://{BUCKET_NAME}/{{{{ ds }}}}/'
             CREDENTIALS = (AWS_KEY_ID = '{{{{ var.value.AWS_ACCESS_KEY }}}}' AWS_SECRET_KEY = '{{{{ var.value.AWS_SECRET_KEY }}}}')
-            FILE_FORMAT = (TYPE = 'PARQUET');
-        """,
+            FILE_FORMAT=(TYPE='PARQUET')""",
         autocommit=True,
     )
 
     # # 2. 스테이지 파일 확인
-    # stage_data_to_snowflake = SnowflakeOperator(
-    #     task_id='stage_data_to_snowflake',
-    #     sql=f"LIST @{SNOWFLAKE_STAGE};",
-    #     snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    #     autocommit=True,
-    # )
+    stage_data_to_snowflake = SnowflakeOperator(
+        task_id="stage_data_to_snowflake",
+        sql=f"LIST @{SNOWFLAKE_SCHEMA}.{SNOWFLAKE_STAGE};",
+        snowflake_conn_id="conn_snowflake_2",
+        autocommit=True,
+    )
 
     # # 3. 스테이지에서 Snowflake 테이블로 데이터 복사
-    # copy_into_snowflake = SnowflakeOperator(
-    #     task_id='copy_into_snowflake',
-    #     sql=f"""
-    #         COPY INTO {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.TFT_BRONZE_TABLE
-    #         FROM @{SNOWFLAKE_STAGE}/
-    #         FILE_FORMAT = (TYPE = 'PARQUET');
-    #     """,
-    #     snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    #     autocommit=True,
-    # )
+    copy_into_snowflake = SnowflakeOperator(
+        task_id="copy_into_snowflake",
+        sql=f"""
+            COPY INTO {SNOWFLAKE_DATABASE}.{SNOWFLAKE_SCHEMA}.TFT_BRONZE_TABLE
+            FROM (
+                SELECT
+                's3://{BUCKET_NAME}/{{{{ ds }}}}' AS source,
+                CURRENT_TIMESTAMP() AS ingestion_date,
+                $1 data
+                FROM @{SNOWFLAKE_SCHEMA}.{SNOWFLAKE_STAGE}
+            )
+            FILE_FORMAT = (TYPE = 'PARQUET');
+        """,
+        snowflake_conn_id="conn_snowflake_2",
+        autocommit=True,
+    )
 
     # # 4. 스테이지 내부 파일 정리
-    # clean_stage_data = SnowflakeOperator(
-    #     task_id="clean_stage_data",
-    #     sql=f"REMOVE @{SNOWFLAKE_STAGE};",
-    #     snowflake_conn_id=SNOWFLAKE_CONN_ID,
-    #     autocommit=True,
-    # )
+    clean_stage_data = SnowflakeOperator(
+        task_id="clean_stage_data",
+        sql=f"REMOVE @{SNOWFLAKE_SCHEMA}.{SNOWFLAKE_STAGE};",
+        snowflake_conn_id="conn_snowflake_2",
+        autocommit=True,
+    )
 
     # 작업 순서 정의
-    # (
-    #     load_data_to_stage
-    #     >> stage_data_to_snowflake
-    #     >> copy_into_snowflake
-    #     >> clean_stage_data
-    # )
-
-    load_data_to_stage
+    (
+        load_data_to_stage
+        >> stage_data_to_snowflake
+        >> copy_into_snowflake
+        >> clean_stage_data
+    )
