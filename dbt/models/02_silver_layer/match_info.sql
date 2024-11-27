fe6180b488eefdbdaffd189e6480e5bb971ccbb7
CoffeeIsNak <tjdqnzo@naver.com>
2024-11-28 05:00:51 +0900

    commit for pull

---
+++ dbt/models/02_silver_layer/match_info.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='incremental', unique_key='match_id') }}
+
+WITH bronze_data AS (
+    SELECT 
+      * 
+    FROM 
+      {{ ref('raw_data_src') }}
+)
+SELECT DISTINCT
+    json_data:metadata:match_id::STRING AS match_id,
+    json_data:info:tft_set_number::INTEGER AS tft_set_number,
+    TO_TIMESTAMP(json_data:info:game_datetime::INTEGER / 1000) AS game_datetime,
+    SUBSTRING(json_data:info:tft_game_type, 1, LENGTH(json_data:info:tft_game_type) - 1) AS tft_game_type,
+    SUBSTRING(json_data:info:tft_set_core_name, 1, LENGTH(json_data:info:tft_set_core_name) - 1) AS tft_set_core_name,
+    CURRENT_TIMESTAMP() AS created_at
+FROM bronze_data
