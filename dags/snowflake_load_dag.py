717e3aa0dc01425cde31abeaa6638df50dc007d8
cbbsjj <56015193+cbbsjj0314@users.noreply.github.com>
2024-11-28 15:05:58 +0900

    temp

--- dags/snowflake_load_dag.py
+++ dags/snowflake_load_dag.py
@@ -3,19 +3,68 @@ from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
 from airflow.models import Variable
 from datetime import datetime, timedelta
 
+from airflow import settings
+from airflow.models import Connection
+import json
+
 # 환경 변수 가져오기
 #SNOWFLAKE_CONN_ID = Variable.get("SNOWFLAKE_CONN_ID", default_var="snowflake_default") 
+SNOWFLAKE_ACCOUNT_ID = Variable.get("SNOWFLAKE_ACCOUNT_ID", default_var=None)
 SNOWFLAKE_WAREHOUSE = Variable.get("SNOWFLAKE_WAREHOUSE", default_var=None)
-SNOWFLAKE_DATABASE = Variable.get("SNOWFLAKE_DATABASE", default_var=None)
-SNOWFLAKE_SCHEMA = Variable.get("SNOWFLAKE_SCHEMA", default_var=None)
-SNOWFLAKE_STAGE = Variable.get("SNOWFLAKE_STAGE", default_var=None)
 SNOWFLAKE_ROLE = Variable.get("SNOWFLAKE_ROLE", default_var=None)
+SNOWFLAKE_ACCOUNT_NAME = Variable.get("SNOWFLAKE_ACCOUNT_NAME", default_var=None)
+SNOWFLAKE_DATABASE = Variable.get("SNOWFLAKE_DATABASE", default_var=None)
+SNOWFLAKE_ACCOUNT_PW = Variable.get("SNOWFLAKE_ACCOUNT_PW", default_var=None)
+SNOWFLAKE_SCHEMA = Variable.get("SNOWFLAKE_SCHEMA", default_var=None)
+# SNOWFLAKE_CONN_ID = Variable.get("SNOWFLAKE_CONN_ID", default_var="snowflake_default")
+SNOWFLAKE_STAGE = Variable.get("SNOWFLAKE_STAGE", default_var=None)
 BUCKET_NAME = Variable.get("BUCKET_NAME", default_var=None)
 AWS_ACCESS_KEY = Variable.get("AWS_ACCESS_KEY", default_var=None)
 AWS_SECRET_KEY = Variable.get("AWS_SECRET_KEY", default_var=None)
 
+SNOWFLAKE_CONN_ID = "snowflake_default"
+
 # 임시 Snowflake_conn_id
-SNOWFLAKE_CONN_ID="conn_snowflake_2"
+# SNOWFLAKE_CONN_ID="conn_snowflake_2"
+
+def create_snowflake_connection():
+    print(f"SNOWFLAKE_ACCOUNT_ID: {SNOWFLAKE_ACCOUNT_ID}")
+    print(f"SNOWFLAKE_WAREHOUSE: {SNOWFLAKE_WAREHOUSE}")
+    print(f"SNOWFLAKE_ROLE: {SNOWFLAKE_ROLE}")
+    print(f"SNOWFLAKE_DATABASE: {SNOWFLAKE_DATABASE}")
+    print(f"SNOWFLAKE_CONN_ID: {SNOWFLAKE_CONN_ID}")
+    print(f"SNOWFLAKE_ACCOUNT_NAME: {SNOWFLAKE_ACCOUNT_NAME}")
+    print(f"SNOWFLAKE_ACCOUNT_PW: {SNOWFLAKE_ACCOUNT_PW}")
+    print(f"SNOWFLAKE_SCHEMA: {SNOWFLAKE_SCHEMA}")
+
+    extra = {
+        "account": SNOWFLAKE_ACCOUNT_ID,
+        "warehouse": SNOWFLAKE_WAREHOUSE,
+        "role": SNOWFLAKE_ROLE,
+        "database": SNOWFLAKE_DATABASE
+    }
+
+    session = settings.Session()
+    existing_connection = session.query(Connection).filter(Connection.conn_id == SNOWFLAKE_CONN_ID).first()
+
+    if not existing_connection:
+        connection = Connection(
+            conn_id=SNOWFLAKE_CONN_ID,
+            conn_type=SNOWFLAKE_CONN_ID,
+            login=SNOWFLAKE_ACCOUNT_NAME,
+            password=SNOWFLAKE_ACCOUNT_PW,
+            schema=SNOWFLAKE_SCHEMA,
+            extra=json.dumps(extra)
+        )
+        session.add(connection)
+        session.commit()
+        print("Connections 설정 완료")
+    else:
+        print("이미 동일한 conn_id의 Connections가 존재함")
+
+    session.close()
+
+create_snowflake_connection()
 
 # DAG 기본 설정
 default_args = {
